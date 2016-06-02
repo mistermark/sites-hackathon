e4edbb42b0ae8f0bc98c5233dea0dabb1324548e
Mark de Jong <mark@markdejong.com>
2016-06-02 11:17:06 +0200

    Updating countdown to count down

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,97 +3,80 @@
   'use strict';
 
   var config = {
-    "date": [{
-      "title": "Day 1",
-      "day": 24,
-      "month": "Jun",
-      "year": "2016",
-      "sup": "th"
+    'countdownClock': document.getElementsByClassName('countdown-clock'),
+    'date': [{
+      'name': 'Day 1',
+      'time': '09:00',
+      'day': 24,
+      'month': 'June',
+      'year': '2016',
+      'sup': 'th'
     },{
-      "title": "Day 2",
-      "day": 25,
-      "month": "Jun",
-      "year": "2016",
-      "sup": "th"
+      'name': 'Day 2',
+      'time': '09:00',
+      'day': 25,
+      'month': 'June',
+      'year': '2016',
+      'sup': 'th'
     }]
   };
 
-  // Spit HTML coming from contents.json
-  // Handlebars.registerHelper('strip-scripts', function(context) {
-  //   return new Handlebars.SafeString(context);
-  // });
-
-  // Get JSON
-  function _loadJSON(url, callback) {
-    var xobj = new XMLHttpRequest();
-
-    xobj.overrideMimeType('application/json');
-    xobj.open('GET', url, true);
-    xobj.onreadystatechange = function () {
-      if (xobj.readyState === 4 && xobj.status === 200) {
-        callback(JSON.parse(xobj.responseText));
-      }
-    };
-    xobj.send(null);
-  }
-
-  function _getWindowSize(cb) {
-
-    $('#video-carousel').width(window.innerWidth);
-    $('#video-carousel').height(window.innerHeight);
-
-    if(cb) {
-      cb();
-    }
-
-  }
+  // function _getWindowSize(cb) {
+  //
+  //   $('#video-carousel').width(window.innerWidth);
+  //   $('#video-carousel').height(window.innerHeight);
+  //
+  //   if(cb) {
+  //     cb();
+  //   }
+  //
+  // }
 
   // Countdown
-  function timeLeft(targetDate) {
-    var currentDate = new Date().getTime(),
-        secondsLeft = (targetDate - currentDate) / 1000,
-        result = {
-          days: 0,
-          hours: 0,
-          minutes: 0,
-          seconds: 0
-        };
+  var _timeRemaining = function(startDate) {
+    var targetDate = Date.parse(startDate.time +' '+ startDate.day +' '+ startDate.month +' '+ startDate.year) - Date.parse(new Date());
 
-    result.days = parseInt(secondsLeft / 86400);
-    secondsLeft = secondsLeft % 86400;
-    result.hours = parseInt(secondsLeft / 3600);
-    secondsLeft = secondsLeft % 3600;
-    result.minutes = parseInt(secondsLeft / 60);
-    result.seconds = parseInt(secondsLeft % 60);
+    var seconds = Math.floor( (targetDate/1000) % 60);
+    var minutes = Math.floor( (targetDate/1000/60) % 60 );
+    var hours = Math.floor( (targetDate/(1000*60*60)) % 24 );
+    var days = Math.floor( targetDate/(1000*60*60*24) );
 
-    return result;
-  }
+    return {
+      'total': targetDate,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  };
 
-  function _setTimerHtml(obj, tl) {
-    for (var i = 0; i < obj.length; i++) {
-      obj[i].innerHTML = '<span>' + tl.days +  ' days</span>, ' +
-        '<span>' + tl.hours + ' hours</span>, ' +
-        '<span>' + tl.minutes + ' minutes</span>, ' +
-        '<span>' + tl.seconds + ' seconds</span>';
-    }
-  }
+  var _initClock = function() {
+    var clock = config.countdownClock;
+
+    var _updateClock = function() {
+      var t = _timeRemaining(config.date[0]);
+
+      for (var i = 0; i < clock.length; i++) {
+        clock[i].querySelector('.days').innerHTML = t.days;
+        clock[i].querySelector('.hours').innerHTML = t.hours;
+        clock[i].querySelector('.minutes').innerHTML = t.minutes;
+        clock[i].querySelector('.seconds').innerHTML = t.seconds;
+      }
+
+      if(t.total<=0) {
+        clearInterval(timeInterval);
+      }
+    };
+
+    _updateClock();
+    var timeInterval = setInterval(_updateClock, 1000);
+  };
 
   // Main
   document.addEventListener('DOMContentLoaded', function() {
 
-    // if(document.getElementById('countdown-clock')) {
-    var countdownTimer = document.getElementsByClassName('countdown-clock');
-    if(countdownTimer) {
-      var startDate = config.date[0];
-      var targetDate = new Date(startDate.month + ', ' + startDate.day + ', ' + startDate.year).getTime();
-
-      var tl = timeLeft(targetDate);
-      if (countdownTimer) {
-        _setTimerHtml(countdownTimer, tl);
-      }
-      // if (countdownCounter) {
-      //   _setTimerHtml(countdownCounter, tl);
-      // }
+    if (config.countdownClock) {
+      _initClock();
     }
 
     // _getWindowSize(function() {
